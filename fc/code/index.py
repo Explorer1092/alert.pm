937613b57ee05ed20534e3b1dacfe0eeeadb3918
Bio B <uwwakooobt@iubridge.com>
2023-08-01 18:55:17 +0800

    alertme

--- fc/code/index.py
+++ fc/code/index.py
@@ -13,24 +13,15 @@ def index():
     return '''<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Serverless Devs - Powered By Serverless Devs</title>
-    <link href="https://example-static.oss-cn-beijing.aliyuncs.com/web-framework/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <div class="website">
     <div class="ri-t">
-        <h1>Devsapp</h1>
-        <h2>这是一个 Flask 项目</h2>
-        <span>自豪的通过Serverless Devs进行部署</span>
-        <br/>
-        <p>您也可以快速体验： <br/>
-            • 下载Serverless Devs工具：npm install @serverless-devs/s<br/>
-            • 初始化项目：s init start-flask<br/>
-
-            • 项目部署：s deploy<br/>
-            <br/>
-            Serverless Devs 钉钉交流群：33947367
-        </p>
+        /gitlab/build/
+        /work/gitlab/build/
+        /work/scanner
+        /github/build/
+        curl -X POST -d "msg=hello" http://alert.pm/me
     </div>
 </div>
 </body>
@@ -86,6 +77,13 @@ def personal_github_build():
     last_commit = request.form.get('last_commit')
     # 在这里处理你的逻辑
     return send_dingtalk_msg(f'repo_name {repo_name}，last_commit {last_commit}', 'manager7827') # 给我自己发消息
+
+@app.route('/me', methods=['POST'])
+def me():
+    msg = request.form.get('msg')
+    # 在这里处理你的逻辑
+
+    return send_dingtalk_msg(msg, 'manager7827') # 给我自己发消息
 
 
 @app.route('/work/scanner', methods=['POST'])
